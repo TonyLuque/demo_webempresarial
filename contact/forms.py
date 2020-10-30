cde8f97ae18823eab3c6a28f3e4db25ab653e377
Anthony Luque <kasuru@hotmail.com>
2020-10-30 18:05:38 -0500

    [ADD] improve form's fields

--- contact/forms.py
+++ contact/forms.py
@@ -1,6 +1,12 @@
 from django import forms
 
 class ContactForm(forms.Form):
-    name = forms.CharField(label='Nombre', required=True)
-    email = forms.EmailField(label='Email', required=True)
-    content = forms.CharField(label='Contenido', required=True, widget=forms.Textarea)
+    name = forms.CharField(label='Nombre', required=True, widget=forms.TextInput(
+        attrs={'class':'nombre_clase', 'placeholder': 'Arturo Rivera'} # Se usa para que Django añada las propiedades al crear el html
+    ), min_length=3, max_length=100)
+    email = forms.EmailField(label='Email', required=True, widget=forms.EmailInput(
+        attrs={'placeholder':'demo@demomail.com'}
+    ), min_length=3, max_length=100)
+    content = forms.CharField(label='Contenido', required=True, widget=forms.Textarea(
+        attrs={'class':'nombre_clase', 'rows':3, 'placeholder': 'Escribe tu mensaje'} # Se usa para que Django añada las propiedades al crear el html
+    ), min_length=10, max_length=1000)
