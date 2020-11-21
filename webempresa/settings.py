b1a49c2724a84aaa356b135df6e1be9d5dc7b978
Anthony Luque <kasuru@hotmail.com>
2020-11-21 18:16:20 -0500

    [ADD] static config

--- webempresa/settings.py
+++ webempresa/settings.py
@@ -132,6 +132,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Media config
 
 MEDIA_URL = '/media/'
