c49ca19f4ca7ef5f5280fa2611c55ba4b819ab4b
Anthony Luque <kasuru@hotmail.com>
2020-11-21 18:29:26 -0500

    [ADD] media to url config

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.blog, name="blog"),
     path('category/<int:category_id>/', views.category, name="category"),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
