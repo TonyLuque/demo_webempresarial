d25e5af12e15e4db93cc26b23e90ca09b0566aaa
Anthony Luque <kasuru@hotmail.com>
2020-11-21 18:35:51 -0500

    [FIX] media to url config

--- services/urls.py
+++ services/urls.py
@@ -1,9 +1,6 @@
 from django.urls import path
 from . import views
-from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.services, name="services"),
 ]
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
