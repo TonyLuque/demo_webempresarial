7b0f15b3f5dc6ab8fa657862e6c1fc1bb8e2325b
Anthony Luque <kasuru@hotmail.com>
2020-12-08 19:14:57 -0500

    [ADD] icons

--- webempresa/settings.py
+++ webempresa/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get('DEBUG'))
 
-ALLOWED_HOSTS = ['demo-webempresarial.herokuapp.com','localhost']
+ALLOWED_HOSTS = ['demo-webempresarial.herokuapp.com','localhost','*']
 
 
 # Application definition
